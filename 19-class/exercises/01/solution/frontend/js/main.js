af45dd9ab0bd980129cc3d92d06fb54d479f5974
Pryann <pryann@gmail.com>
2022-08-29 15:19:14 +0200

    update repo

--- 19-class/exercises/01/solution/frontend/js/main.js
+++
@@ -1,4 +0,0 @@
-import User from './User.js';
-
-// Itt lehet hívogatni a metódusokat
-User.read(1);
--- 19-class/exercises/03/solution/frontend/js/main.js
+++
@@ -1,9 +0,0 @@
-import Api from './Api.js';
-import Messenger from './Messenger.js';
-import User from './User.js';
-
-const usersMessenger = new Messenger();
-const usersApi = new Api('users');
-const user = new User(usersApi, usersMessenger);
-
-user.read(1);
